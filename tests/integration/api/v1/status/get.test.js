fc8c50d5cfc654ab09da24046c956f1630420feb
Geanderson-Ferreira <74616054+Geanderson-Ferreira@users.noreply.github.com>
2025-11-26 01:12:50 +0000

    Finalizado endpoint de status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("/api/v1/status should be 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  responseBody = await response.json();
+
+  // Assertions
+
+  //updated_at
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  //.dependencies.database.version
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  //.dependencies.database.max_connections
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  console.log(responseBody);
 });
