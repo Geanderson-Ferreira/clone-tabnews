ed4cb35997ca8f28ba897c73339ec7910f7a754d
Geanderson-Ferreira <74616054+Geanderson-Ferreira@users.noreply.github.com>
2025-11-29 20:11:24 +0000

    Adicionado conexão ssl ao banco

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,21 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_host,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
+    ssl: true,
+  });
+
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    ssl: true,
   });
 
   try {
@@ -15,6 +25,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.error(error);
+    throw new Error(error);
   } finally {
     await client.end();
   }
